77071a846b5e6bc6ea97eea1312aa19a4cba302c
anonim-24 <akbaralfa024@gmail.com>
2025-03-30 15:48:58 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("cta-button").addEventListener("click", function () {
+    alert("Terima kasih telah mendaftar di Brain Master! Kami akan menghubungi Anda segera.");
+});
+
+// Form Submission Event
+document.getElementById("registration-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Pendaftaran Anda berhasil! Kami akan menghubungi Anda segera.");
+});
